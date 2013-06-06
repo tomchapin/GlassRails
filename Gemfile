42684e8989d45ecc8663463896144294fa2bdbae
Tom Chapin <tchapin@gmail.com>
2013-06-06 15:39:18 -0600

    google auth config

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ end
 
 group :development, :test do
   gem "sqlite3"
+  gem 'dotenv-rails'
 end
 
 gem "google-api-client"
--- config/initializers/google_auth.rb
+++
@@ -1,4 +0,0 @@
-GOOGLE_AUTH_CONFIG = YAML.load_file("#{::Rails.root}/config/google_auth.yml")[::Rails.env]
-
-ENV['GOOGLE_KEY']    = GOOGLE_AUTH_CONFIG['client_id']
-ENV['GOOGLE_SECRET'] = GOOGLE_AUTH_CONFIG['secret']
